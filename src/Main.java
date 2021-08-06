8eb6bcd689ee50f05560050c1414f998fc1a7e73
Yana Mishina <Lion_best85@list.ru>
2021-08-07 01:38:18 +0300

    Added Card JCB

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "4556197657252165136";
+        String number = "3529719870404492533";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
